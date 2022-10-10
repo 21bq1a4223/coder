068a55ed3e8239f5e0552c3a51135fc91d2ec65a
21bq1a4223 <113083251+21bq1a4223@users.noreply.github.com>
2022-10-10 12:04:01 +0530

    Add files via upload

---
+++ names.py
@@ -0,0 +1,8 @@
+names={}
+n=int(input("enter number of names:"))
+for i in range(n):
+    names[i+1]=input("enter a name:")
+name=list(names.values())
+p=int(input("enter position:"))
+name.sort(key_lambdaX.X[p])
+print(name)
