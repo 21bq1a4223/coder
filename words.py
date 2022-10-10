1ddd1b18964462ffa157c6f50d5c4e09125345a1
21bq1a4223 <113083251+21bq1a4223@users.noreply.github.com>
2022-10-10 10:47:25 +0530

    Add files via upload

--- words.py
+++ words.py
@@ -1,10 +1,11 @@
-names=['arun','varun','kent','eat','pot','net','peak','peacock','zebra','nato','toe','poke',knife','peot','venus','ant']
+names=['Arun','Varun','kent','eat','pot','net','peak','peacock','zebra','nato','toe','poke','knife','peot','venus','ant']
 letters=['A','K','E','O','T','P','N']
 for name in names:
-    c=0
-    l=list(set(name.upper()))
-    for letter in l:
-        if letter in letter:
-            c+=1
-        if c==len(name):
-            print(names)
+    cnt=0
+    uni=list(set(name.upper()))
+    for letter in uni:
+        if letter in letters:
+            cnt+=1
+    if cnt==len(name):
+        print(name)
+    
